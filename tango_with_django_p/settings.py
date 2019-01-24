1c0a53b6b23de6a9a98d66e1241def706a6816bd
Raghad  abdulrab <raghadabdulrab@Raghads-MacBook-Pro.local>
2019-01-24 16:59:03 +0000

    sixth commit

--- tango_with_django_p/settings.py
+++ tango_with_django_p/settings.py
@@ -63,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -121,3 +122,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR, ]
+
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
--- tango_with_django_p/urls.py
+++ tango_with_django_p/urls.py
@@ -17,6 +17,9 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls import include
 from rango import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
 url(r'^$', views.index, name='index'),
@@ -25,7 +28,9 @@ url(r'^rango/', include('rango.urls')), # above maps any URLs starting
 # the rango application
 
 url(r'^admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
 
 # above maps any URLs starting
 # with rango/ to be handled by
