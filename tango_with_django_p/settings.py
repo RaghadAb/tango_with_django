22f8d5efa745430b5257815c60c2a00c7e4ff127
Raghad  abdulrab <raghadabdulrab@visitor17.gu-visitors.net>
2019-01-25 09:33:01 +0000

    ninth commit

--- tango_with_django_p/settings.py
+++ tango_with_django_p/settings.py
@@ -126,3 +126,10 @@ STATICFILES_DIRS = [STATIC_DIR, ]
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
