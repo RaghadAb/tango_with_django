69ea9d8bbdd6d95f6bbf4135cc0fc9b90af818cb
Raghad  abdulrab <raghadabdulrab@Raghads-MacBook-Pro.local>
2019-02-02 11:04:59 +0000

    14

--- rango/admin.py
+++ rango/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
+from rango.models import Category
 
-# Register your models here.
 
 from rango.models import Category, Page
 admin.site.register(Category)
+
 admin.site.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url')
 
--- rango/models.py
+++ rango/models.py
@@ -8,7 +8,7 @@ class Category(models.Model):
     name = models.CharField(max_length=length_max, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(blank=True,unique=True)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
@@ -35,16 +35,4 @@ class Page(models.Model):
     def __unicode__(self):
         return self.title
 
-class UserProfile(models.Model):
-    # This line is required. Links UserProfile to a User model instance.
-    user = models.OneToOneField(User, on_delete=models.DO_NOTHING,)
 
-    # The additional attributes we wish to include.
-    website = models.URLField(blank=True)
-    picture = models.ImageField(upload_to='profile_images', blank=True)
-
-    def __str__(self):
-        return self.user.username
-
-    def __unicode__(self):
-        return self.user.username
